7aeb508532fffb6efbadd6a02264aba4558f6e37
widrns15 <85780501+widrns15@users.noreply.github.com>
2023-12-03 20:10:15 +0900

    🚀 build: [Vite] gh-pages 배포 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // ex base: '/my_project.git/',
+  base: "web_profile",
   plugins: [react()],
-})
+});
